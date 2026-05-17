a541d7e0c67d8e9648e333125ea1f70c2cf371a5
Damon <pvdoriginal2019@gmail.com>
2026-05-17 04:05:08 +0300

    better queries

--- src/MischiefECS/Query.hs
+++ src/MischiefECS/Query.hs
@@ -12,17 +12,18 @@ import Data.Proxy
 class QueryData qd where 
     types :: qd -> Set TypeRep
 
-instance (Component c) => QueryData (Proxy c) where
+instance {-# OVERLAPPABLE #-} (Component c) => QueryData (Proxy c) where
     types :: Component c => Proxy c -> Set TypeRep
     types _ = Set.singleton . typeRep $ Proxy @c 
 
-instance (QueryData a0, QueryData a1) => QueryData (a0, a1) where
-    types :: (QueryData a0, QueryData a1) => (a0, a1) -> Set TypeRep
-    types (a0, a1) = Set.union (types a0) (types a1)
-
-class QueryDataExternal 
+instance (QueryData (Proxy a0), QueryData (Proxy a1)) => QueryData (Proxy (a0, a1)) where
+    types :: (QueryData (Proxy a0), QueryData (Proxy a1)) => Proxy (a0, a1) -> Set TypeRep
+    types _ = Set.union (types $ Proxy @a0) (types $ Proxy @a1)  
 
 data Query qd = (QueryData qd) => Query qd
 
 queryTypes :: Query qd -> Set TypeRep 
 queryTypes (Query qd) = types qd  
+
+fillQuery :: (Recoverable c e) => Query (Proxy c) -> e -> Maybe c  
+fillQuery _ e = recover e 
