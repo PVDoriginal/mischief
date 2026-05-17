3474e0e75555695f38348ad3207161d12766a11b
Damon <pvdoriginal2019@gmail.com>
2026-05-18 00:17:37 +0300

    Update Main.hs

--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,6 @@
 module Main where
 
 import MischiefECS
-import Data.IORef (readIORef)
-import Data.Typeable
 
 data C1 = C1 Int Int deriving (Component, Show) 
 data C2 = C2 String deriving (Component, Show) 
@@ -12,18 +10,18 @@ data C4 = C4 Int String deriving (Component, Show)
 
 main :: IO ()
 main = do 
-    world <- newWorld
-    
-    e1 <- spawnEntity (C1 2 3) world    
-    e2 <- spawnEntity (C1 5 2) world    
-    e3 <- spawnEntity (C1 1 3) world        
-    e4 <- spawnEntity (C2 "lmao", C1 1 1) world    
-    e5 <- spawnEntity (C1 5 9, C2 "lol") world    
-    e6 <- spawnEntity (C2 "haha", C3 5.3) world
-    e7 <- spawnEntity (C1 9 9, C2 "ugh", C3 4.1) world
-    e8 <- spawnEntity (C3 9.4, C2 "eh") world
+  world <- newWorld
+  
+  e1 <- spawnEntity (C1 2 3) world    
+  e2 <- spawnEntity (C1 5 2) world    
+  e3 <- spawnEntity (C1 1 3) world        
+  e4 <- spawnEntity (C2 "lmao", C1 1 1) world    
+  e5 <- spawnEntity (C1 5 9, C2 "lol") world    
+  e6 <- spawnEntity (C2 "haha", C3 5.3) world
+  e7 <- spawnEntity (C1 9 9, C2 "ugh", C3 4.1) world
+  e8 <- spawnEntity (C3 9.4, C2 "eh") world
 
-    putStrLn "\n"
+  putStrLn "\n"
 
-    c <- query (type (C2)) world
-    putStrLn $ show c 
+  c <- query (type (C2)) world
+  putStrLn $ show c 
