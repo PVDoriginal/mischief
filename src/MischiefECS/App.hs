b7706ffdc3c8d29db6d969847dcecc2bb7d95ce7
Damon <pvdoriginal2019@gmail.com>
2026-05-18 23:11:41 +0300

    app prototype

--- src/MischiefECS/App.hs
+++ src/MischiefECS/App.hs
@@ -1,8 +1,25 @@
 module MischiefECS.App where
 
+import Control.Monad (forM)
+import Control.Monad.Trans.Reader
+import Data.Foldable
+import Data.IORef
 import MischiefECS.World
 
-data App = App {systems :: [System ()]}
+data App = App {systems :: IORef [System ()], world :: World}
 
-newApp :: App
-newApp = App {systems = []}
+newApp :: IO App
+newApp = do
+  world <- newWorld
+  systems <- newIORef []
+  return App {systems, world}
+
+addSystem :: System () -> App -> IO ()
+addSystem system App {systems} = do
+  modifyIORef' systems (++ [system])
+
+runApp :: App -> IO ()
+runApp app = do
+  systems <- readIORef app.systems
+  for_ systems $ \system ->
+    runReaderT system app.world
--- test/Main.hs
+++ test/Main.hs
@@ -14,26 +14,26 @@ data C4 = C4 Int String deriving (Component, Show)
 
 main :: IO ()
 main = do
-  world <- newWorld
-  runReaderT system world
+  app <- newApp
+  addSystem system app
+  addSystem system2 app
+  runApp app
 
 system :: System ()
 system = do
   e1 <- spawn (C1 5 5, C2 "Lmfao")
   insert (C3 5.3) e1
 
-  res1 <- query @(C2, C3)
-  liftIO $ putStrLn $ show res1
+  res2 <- query @(C2, C3)
+  liftIO $ putStrLn $ show res2
 
-  remove (type C2) e1
-
+system2 :: System ()
+system2 = do
   res2 <- query @(C2, C3)
   liftIO $ putStrLn $ show res2
 
   res3 <- query @C1
   liftIO $ putStrLn $ show res3
-
-  despawn e1
 
   res4 <- query @C1
   liftIO $ putStrLn $ show res4
