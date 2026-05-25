2b81ccae9a8c0794eb5f98ad74dd546ff440e931
Damon <pvdoriginal2019@gmail.com>
2026-05-26 02:28:43 +0300

    Update MischiefECS.hs

--- src/MischiefECS.hs
+++ src/MischiefECS.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
 module MischiefECS
   ( module MischiefECS.Tables,
     module MischiefECS.World,
@@ -11,6 +13,7 @@ module MischiefECS
   )
 where
 
+import Data.Data
 import MischiefECS.App
 import MischiefECS.Components
 import MischiefECS.Components.Bundle
@@ -20,3 +23,22 @@ import MischiefECS.Tables
 import MischiefECS.World
 import MischiefECS.World.Query
 import MischiefECS.World.Remove
+
+class SystemParam s where
+  type Result s
+
+  eval :: Proxy s -> Result s
+
+instance (Queryable qd) => SystemParam qd where
+  type Result qd = QueryOutput qd
+
+  eval :: forall k (qd :: k). (QueryData qd) => Proxy qd -> Result qd
+  eval = undefined
+
+system :: forall s. (SystemParam s) => (Result s -> System ()) -> System ()
+system = undefined
+
+test :: System ()
+test =
+  system @(Entity, Name) $ \(entity, name) -> do
+    insert (Name "Lol") entity
