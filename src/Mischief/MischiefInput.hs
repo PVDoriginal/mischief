68a2955b8c42640005698c3807f74ce3b858611e
Damon <pvdoriginal2019@gmail.com>
2026-05-27 21:21:16 +0300

    Update MischiefInput.hs

--- src/Mischief/MischiefInput.hs
+++ src/Mischief/MischiefInput.hs
@@ -1,1 +1,15 @@
 module MischiefInput where
+
+import Control.Monad.IO.Class
+import MischiefECS
+import System.IO
+
+data Key = Space | Enter deriving (Show, Eq)
+
+inputPlugin :: Plugin ()
+inputPlugin = do
+  liftIO $ hSetBuffering stdin NoBuffering
+
+readInput :: System ()
+readInput = do
+  undefined
