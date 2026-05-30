d520992c622290e34b1db0f49895aeaca27b3040
Damon <pvdoriginal2019@gmail.com>
2026-05-30 20:02:10 +0300

    Update Events.hs

--- mischief-ecs/src/MischiefECS/Events.hs
+++ mischief-ecs/src/MischiefECS/Events.hs
@@ -3,20 +3,22 @@ module MischiefECS.Events where
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Reader (ReaderT (runReaderT), ask)
 import Data.Data (Typeable)
+import Data.Foldable (for_)
+import Data.IORef (modifyIORef', readIORef, writeIORef)
 import MischiefECS.App
 import MischiefECS.Components
 import MischiefECS.Events.Internal
-import MischiefECS.World (System, spawn)
+import MischiefECS.Tables
+import MischiefECS.World
+import MischiefECS.World.Query
 
-data EventProxy e = EventProxy
+data EventProxy e = EventProxy deriving (Component, Queryable)
 
 class (Typeable e) => Event e where
   eraseEvent :: e -> ErasedEvent
   eraseEvent = ErasedEvent
 
-instance (Event e) => Component (EventProxy e)
-
-newtype Observer e = Observer (e -> System ()) deriving (Component)
+newtype Observer e = Observer (e -> System ()) deriving (Component, Queryable)
 
 addObserver :: (Event e) => (e -> System ()) -> Plugin ()
 addObserver observer = do
@@ -27,3 +29,22 @@ spawnObserver :: forall e. (Event e) => Observer e -> System ()
 spawnObserver observer = do
   _ <- spawn (observer, EventProxy @e)
   return ()
+
+trigger :: (Event e) => e -> System ()
+trigger event = do
+  world <- ask
+  liftIO $ modifyIORef' world.events (++ [eraseEvent event])
+
+runEvent :: ErasedEvent -> System ()
+runEvent (ErasedEvent (event :: e)) = do
+  observers <- query @(Observer e, EventProxy e)
+  for_ observers $ \(observer, _) -> do
+    let Observer f = observer.value
+    f event
+
+flushEvents :: System ()
+flushEvents = do
+  world <- ask
+  events <- liftIO $ readIORef world.events
+  for_ events runEvent
+  liftIO $ writeIORef world.events []
