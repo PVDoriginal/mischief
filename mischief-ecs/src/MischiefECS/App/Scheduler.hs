7419302f438695b8657147531596748f69f2aee7
Damon <pvdoriginal2019@gmail.com>
2026-06-03 14:41:11 +0300

    Update Scheduler.hs

--- mischief-ecs/src/MischiefECS/App/Scheduler.hs
+++ mischief-ecs/src/MischiefECS/App/Scheduler.hs
@@ -1,9 +1,19 @@
 module MischiefECS.App.Scheduler where
 
 import Data.Data
+import Data.Map (Map)
+import MischiefECS.Graph (Graph)
+import MischiefECS.Graph qualified as Graph
 import MischiefECS.World
 
-data Scheduler = Undefined
+data Scheduler = Scheduler
+  { schedules :: Graph ScheduleLabel,
+    systems :: Map ScheduleLabel (Graph SystemSetId)
+  }
+
+newtype SystemSetId = SystemSetId Int
+
+newtype ScheduleLabel = ScheduleLabel {rep :: TypeRep} deriving (Eq, Ord, Show)
 
 data ScheduleGraph = Undefined1
 
