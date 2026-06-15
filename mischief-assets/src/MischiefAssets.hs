235100699c8146833b2f98d63847cda82ab4b494
Damon <pvdoriginal2019@gmail.com>
2026-06-15 23:41:11 +0300

    assets prototyping

--- mischief-assets/src/MischiefAssets.hs
+++ mischief-assets/src/MischiefAssets.hs
@@ -1,1 +1,7 @@
 module MischiefAssets where
+
+import Data.ByteString (ByteString)
+import GHC.IO.Handle
+
+class Asset a where
+  load :: ByteString -> a
--- mischief-assets/test/Main.hs
+++ mischief-assets/test/Main.hs
@@ -1,4 +1,28 @@
 module Main where
 
+import Control.Concurrent.Async
+import Control.Monad.IO.Class
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as B
+import MischiefECS
+import System.IO
+
+data Dummy = Dummy ByteString deriving (Component, Queryable)
+
 main :: IO ()
-main = return ()
+main = do
+  app <- newApp [plugin]
+  runApp app
+
+plugin :: Plugin ()
+plugin = do
+  addSystems Startup printString
+
+printString :: System ()
+printString = do
+  liftIO $ print "AAA"
+  contents <- liftIO $ async $ B.readFile "a.txt"
+  -- _ <- spawn (Dummy contents)
+  liftIO $ print "BBB"
+
+-- liftIO $ print contents
