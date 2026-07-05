ac7aa65fb039555181b5a732e648465bd2cf8aef
Damon <pvdoriginal2019@gmail.com>
2026-07-06 02:29:39 +0300

    Update Systems.hs

--- mischief-ecs/src/MischiefECS/App/Systems.hs
+++ mischief-ecs/src/MischiefECS/App/Systems.hs
@@ -21,10 +21,8 @@ import MischiefECS.World.Internal
 import MischiefECS.World.Query
 import MischiefECS.World.Spawn
 
-data Systems = Systems
-  { systemMap :: IORef (Map (ScheduleLabel, Int) (IORef [(StableName (System ()), SystemId)])),
-    systemData :: IORef (Map SystemId (System (), IORef Tick)),
-    counter :: IORef Int
+newtype Systems = Systems
+  { systemMap :: IORef (Map (ScheduleLabel, Int) (IORef [(StableName (System ()), SystemId)]))
   }
 
 newtype SystemFunction = SystemFunction {inner :: System ()} deriving anyclass (Component)
@@ -36,42 +34,29 @@ newtype LastSystemTick = LastSystemTick {inner :: Tick} deriving anyclass (Compo
 newSystems :: IO Systems
 newSystems = do
   systemMap <- newIORef Map.empty
-  systemData <- newIORef Map.empty
-  counter <- newIORef 0
-  return Systems {systemMap, systemData, counter}
+  return Systems {systemMap}
 
-getSystemId' :: System () -> StableName (System ()) -> IORef [(StableName (System ()), SystemId)] -> IORef Int -> IORef (Map SystemId (System (), IORef Tick)) -> System SystemId
-getSystemId' system stableName list counter systemData = do
+getSystemId' :: System () -> StableName (System ()) -> IORef [(StableName (System ()), SystemId)] -> System SystemId
+getSystemId' system stableName list = do
   list' <- liftIO $ readIORef list
   case find (\x -> fst x `eqStableName` stableName) list' of
     Just (_, x) -> return x
     Nothing -> do
       index <- spawn (SystemFunction system, (SystemTick (Tick 0), LastSystemTick (Tick 0)))
       liftIO $ modifyIORef' list (++ [(stableName, SystemId index)])
-      -- tick <- newIORef $ Tick 0
-      -- modifyIORef' systemData $ Map.insert (SystemId index) (system, tick)
-
       return $ SystemId index
 
 getSystemId :: ScheduleLabel -> System () -> Systems -> System SystemId
-getSystemId label system Systems {systemMap, counter, systemData} = do
+getSystemId label system Systems {systemMap} = do
   stableName <- liftIO $ makeStableName system
   systemMap' <- liftIO $ readIORef systemMap
 
   case Map.lookup (label, hashStableName stableName) systemMap' of
-    Just x -> getSystemId' system stableName x counter systemData
+    Just x -> getSystemId' system stableName x
     Nothing -> do
       l <- liftIO $ newIORef []
       liftIO $ modifyIORef' systemMap (Map.insert (label, hashStableName stableName) l)
-      getSystemId' system stableName l counter systemData
-
-getSystemData :: SystemId -> Systems -> System (System (), Tick)
-getSystemData systemId Systems {systemData} = do
-  -- systemData <- liftIO $ readIORef systemData
-  -- maybe undefined return (Map.lookup systemId systemData)
-  Just f <- get @SystemFunction systemId.entity
-  Just t <- get @SystemTick systemId.entity
-  return (f.inner, t.inner)
+      getSystemId' system stableName l
 
 getSystemTicks :: World -> IO (Tick, Tick)
 getSystemTicks world = do
