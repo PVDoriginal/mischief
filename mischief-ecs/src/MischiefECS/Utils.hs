88c6a50dc0ffa4823598cdc26a814185d0e374bd
Damon <pvdoriginal2019@gmail.com>
2026-07-05 16:41:47 +0300

    Update Utils.hs

--- mischief-ecs/src/MischiefECS/Utils.hs
+++ mischief-ecs/src/MischiefECS/Utils.hs
@@ -4,12 +4,5 @@ import Data.Foldable
 import MischiefECS.World.Internal
 import MischiefECS.World.Par
 
-iter :: (MonadSystem w m, Foldable t) => m (t a) -> (a -> m b) -> m ()
-iter x s = do
-  x' <- x
-  for_ x' s
-
-parIter :: (MonadSystem w m, Foldable t) => m (t a) -> (a -> ParSystem b) -> m ()
-parIter x s = do
-  x' <- x
-  parIterList x' $ \chunk -> for_ chunk s
+parIter :: (MonadSystem w m, Foldable t) => t a -> (a -> ParSystem b) -> m ()
+parIter x s = parIterList x $ \chunk -> for_ chunk s
