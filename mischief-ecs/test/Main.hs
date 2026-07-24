63eff326a9836ad28efdcb284d33b4b9a81f8c55
Damon <pvdoriginal2019@gmail.com>
2026-07-25 00:26:01 +0300

    Update Main.hs

--- mischief-ecs/test/Main.hs
+++ mischief-ecs/test/Main.hs
@@ -45,7 +45,7 @@ s = do
   b <- spawn ()
   c <- spawn ()
 
-  x <- [q|Name / Check (\(Pos x y) -> y > 5) -> *|]
+  x <- [q|Name / With Pos, !(With CompA, With CompB)|]
   return ()
 
 main :: IO ()
