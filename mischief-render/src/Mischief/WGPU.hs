ec2478d90072ac777912e71a9712a74f343de9c2
Damon <pvdoriginal2019@gmail.com>
2026-08-18 23:29:34 +0300

    more FFI :D

--- mischief-render/src/Mischief/WGPU.hs
+++ mischief-render/src/Mischief/WGPU.hs
@@ -1,1 +1,5 @@
 module Mischief.WGPU where
+
+import Foreign.C.Types
+
+foreign import ccall "webgpu.h wgpuCommandBufferSetLabel" c_test :: CInt -> CInt -> IO ()
--- mischief-render/test/Main.hs
+++ mischief-render/test/Main.hs
@@ -1,21 +1,6 @@
-import Mischief.ECS
-import Mischief.ECS.App
-import Mischief.ECS.SDL
-import MischiefAssets
-import MischiefAssets.Asset
-import MischiefRender
+import Mischief.ECS.Prelude
+import Mischief.WGPU
 
 main :: IO ()
 main = do
-  app <- newApp [assetPlugin, sdlPlugin, renderPlugin, plugin]
-  runApp app
-
-plugin :: Plugin ()
-plugin = do
-  addSystems Startup s
-
-s :: System ()
-s = do
-  _ <- load "assets/test.frag.hlsl"
-  _ <- load "assets/test.vert.hlsl"
-  return ()
+  c_test 5 5
